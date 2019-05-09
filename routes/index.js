b85ade25447bba3bc61a130ba60100bc423626dc
Julia Helena <jh@juliahelena.me>
2019-05-09 15:12:37 -0400

    app starter

---
+++ routes/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { body, validationResult } = require('express-validator/check');
+
+const router = express.Router();
+const Registration = mongoose.model('Registration');
+
+// const path = require('path');
+// const auth = require('http-auth');
+// const basic = auth.basic({
+//     file: path.join(__dirname, '../users.htpasswd'),
+//   });
+
+router.get('/', (req, res) => {
+    res.render('form', { title: 'Registration form' });
+});
+
+router.get('/registrations', (req, res) => {
+    Registration.find()
+      .then((registrations) => {
+        res.render('index', { title: 'Listing registrations', registrations });
+      })
+      .catch(() => { res.send('Sorry! Something went wrong.'); });
+  });
+
+router.post('/', 
+  [
+    body('name')
+      .isLength({ min: 1 })
+      .withMessage('Please enter a name'),
+    body('email')
+      .isLength({ min: 1 })
+      .withMessage('Please enter an email'),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+        const registration = new Registration(req.body);
+        registration.save()
+            .then(() => { res.send('Thank you for your registration!');
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
+    } else {
+        res.render('form', {
+        title: 'Registration form',
+        errors: errors.array(),
+        data: req.body,
+  });
+}
+}
+);
+
+module.exports = router;
+
